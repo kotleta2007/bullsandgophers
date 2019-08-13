9bed092abd4be8508a89043109f9772f063f12ae
Mark Tropin <kotleta2007@protonmail.ch>
2019-08-13 00:20:14 +0000

    Added random generator. Needs time-based seeding

--- src/main.go
+++ src/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+    "math/rand"
 )
 
+func genRand() (r[4] int) {
+    digits := [...] int {0,1,2,3,4,5,6,7,8,9}
+    
+    rand.Shuffle(len(digits), func(i, j int) {
+        digits[i], digits[j] = digits[j], digits[i]
+    })
+    
+    r = [...] int {digits[0], digits[1], digits[2], digits[3]}
+    
+    return
+}
+
 func main() {
-	fmt.Println("First commit")
+	// TODO: Generate random 4-digit number (no repetitions allowed)
+    
+    fmt.Println(genRand())
 }
