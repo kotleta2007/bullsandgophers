2ca0475d91b90663e1e44c49ce3ff30ec7db9de7
Mark Tropin <kotleta2007@protonmail.ch>
2019-08-15 23:20:32 +0000

    Game is working

--- src/main.go
+++ src/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+    "errors"
     "fmt"
     "math/rand"
     "time"
-    "errors"
 )
 
 func genRand() (r[4] int) {
@@ -24,13 +24,13 @@ func genRand() (r[4] int) {
     return
 }
 
-func strToArray(str string) (arr[] int, err error) {
+func strToArray(str string) (arr[4] int, err error) {
     if len(str) != 4 {
-        return nil, errors.New("Invalid string size") 
+        return  [4] int {0, 0, 0, 0}, errors.New("Invalid string size")
     }
     
     for i := 0; i < len(str); i++ {
-        arr = append(arr, int(str[i]) - 48)
+        arr[i] = int(str[i]) - 48
     }
     
     return
@@ -62,11 +62,63 @@ func countCows(arr1[4] int, arr2[4] int) (int) {
     return res
 }
 
-func main() {    
-    a := [...] int {1, 2, 3, 4}
-    b := [...] int {1, 2, 4, 7}
-    
-    fmt.Println("Cows:", countCows(a, b))
-    fmt.Println("Bulls:", countBulls(a, b))
-    // fmt.Println(genRand())
+func game() {
+    tries := 0
+    secret := genRand()
+
+    for {
+        var userTry string
+        fmt.Scanln(&userTry)
+
+        // quit current game
+        if userTry == "Q" || userTry == "q" {
+           return
+        }
+
+        userArray, _ := strToArray(userTry)
+
+        // fmt.Println(secret)
+        // fmt.Println(userTry)
+        // fmt.Println(userArray)
+        // fmt.Println(secret == userArray)
+
+        fmt.Println("Bulls:", countBulls(secret, userArray))
+        fmt.Println("Cows:", countCows(secret, userArray))
+
+
+        tries++
+
+        if (secret == userArray) {
+            break
+        }
+    }
+
+    fmt.Println("You have completed the game in", tries, "tries.")
+
+    return
+}
+
+func main() {
+    var user_choice byte
+
+    for {
+        fmt.Println("\nEnter N to start a new game.")
+        fmt.Println("Enter Q to quit.")
+        fmt.Scanf("%c\n", &user_choice)
+
+        if user_choice == 'N' || user_choice == 'n' {
+            fmt.Println("\n\nNew game!")
+            game()
+            continue
+        } else if user_choice == 'Q' || user_choice == 'q' {
+            break
+        } else {
+            fmt.Println(user_choice, "is not an option. Try again.")
+            continue
+        }
+    }
+
+    fmt.Println("You quit")
+    //fmt.Println("Cows:", countCows(a, b))
+    //fmt.Println("Bulls:", countBulls(a, b))
 }
